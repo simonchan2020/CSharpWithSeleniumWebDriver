aa298f5473c8458a7065f3d966b1cfc173295146
simon <sj102399@gmail.com>
2020-02-28 15:16:34 -0600

    Added base page

--- CSharpWithSeleniumWebDriver/BasePages/GoogleBasePage.cs
+++ CSharpWithSeleniumWebDriver/BasePages/GoogleBasePage.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using CSharpWithSeleniumWebDriver.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
 namespace CSharpWithSeleniumWebDriver.BasePages
 {
     public class GoogleBasePage
     {
-        public GoogleBasePage()
+        private IWebDriver _driver;
+        private SeleniumHelper selenium;
+
+        private By textfieldSearchQuery = By.Name("q");
+
+        public GoogleBasePage(IWebDriver driver)
         {
+            _driver = driver;
+            selenium = new SeleniumHelper(_driver);
+        }
+
+        public void DoSearchFor(string searchQuery)
+        {
+            selenium.SendKeys(textfieldSearchQuery, searchQuery);
+
+            new Actions(_driver).SendKeys(Keys.Enter).Build().Perform();
         }
     }
 }
